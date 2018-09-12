46df83a9e09c2ec0a368a8d08c6cb2e07f1515ec
eermolaev <eermolaev@yandex.ru>
2018-09-12 19:02:37 +0400

    SUB-521

---
+++ Model/Config/Source/Survey/PaymentOptions.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Copyright © 2018 TechNWeb, Inc. All rights reserved.
+ * See TNW_LICENSE.txt for license details.
+ */
+
+namespace TNW\Marketing\Model\Config\Source\Survey;
+
+use TNW\Marketing\Model\Config\Source\Survey\Options\Base;
+
+/**
+ * PaymentOptions
+ */
+class PaymentOptions extends Base
+{
+    /** @var array */
+    public static $optionsDetails = [
+        ['label' => 'I want to mPower my business'],
+        [
+            'label' => 'Contact me later',
+            'timemodifier' => '+1 month',
+        ],
+        ['label' => 'I am happy with the FREE version'],
+        ['label' => 'I am a system integrator and just testing this out']
+    ];
+
+}
